dedf6e453135e5e04fc388349301ca2468f5f330
SIIIKOR <siiikor.sikorski@gmail.com>
2023-07-14 18:54:06 +0200

    intitial state

---
+++ src/test/java/com/msik404/karmaapp/KarmaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msik404.karmaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarmaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
