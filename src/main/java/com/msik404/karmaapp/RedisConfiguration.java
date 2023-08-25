f65f2e2f3a5f5e2642540537433dcab76f76a6ed
SIIIKOR <siiikor.sikorski@gmail.com>
2023-08-25 13:49:08 +0200

    Added byteRedisTemplate bean definition

--- src/main/java/com/msik404/karmaapp/RedisConfiguration.java
+++ src/main/java/com/msik404/karmaapp/RedisConfiguration.java
@@ -3,6 +3,7 @@ package com.msik404.karmaapp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
@@ -12,6 +13,14 @@ public class RedisConfiguration {
     StringRedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
 
         var template = new StringRedisTemplate();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+
+    @Bean
+    RedisTemplate<String, byte[]> byteRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+
+        var template = new RedisTemplate<String, byte[]>();
         template.setConnectionFactory(redisConnectionFactory);
         return template;
     }
