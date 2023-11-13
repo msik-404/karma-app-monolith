bdea7a7537d034a2b015f37d8236a3a2bdc0a070
SIIIKOR <siiikor.sikorski@gmail.com>
2023-11-13 14:53:19 +0100

    Added data init controlled by env variable.

--- src/main/java/com/msik404/karmaappmonolith/init/DataInit.java
+++ src/main/java/com/msik404/karmaappmonolith/init/DataInit.java
@@ -1,7 +1,9 @@
 package com.msik404.karmaappmonolith.init;
 
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 
 import com.msik404.karmaappmonolith.post.Post;
@@ -10,14 +12,24 @@ import com.msik404.karmaappmonolith.post.repository.PostRepository;
 import com.msik404.karmaappmonolith.user.Role;
 import com.msik404.karmaappmonolith.user.User;
 import com.msik404.karmaappmonolith.user.repository.UserRepository;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.lang.NonNull;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 @RequiredArgsConstructor
-public class UserAndPostInit implements CommandLineRunner {
+public class DataInit implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    private static final Logger logger = LoggerFactory.getLogger(DataInit.class);
 
     private static final int USER_AMOUNT = 300;
     private static final int MAX_SINGLE_INSERT_SIZE = 100;
@@ -26,27 +38,55 @@ public class UserAndPostInit implements CommandLineRunner {
     private static final int MIN_POSTS_PER_USER = 3;
     private static final Visibility[] VISIBILITY_OPTIONS = Visibility.class.getEnumConstants();
 
-    private final UserRepository userRepository;
-    private final PostRepository postRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    User getUserForInserting(@NonNull String username) {
+    private static final String ADMIN_USERNAME = "ADMIN";
+    private static final String MOD_USERNAME = "MOD";
+
+    @NonNull
+    private static String getUsername(long id) {
+        return String.format("username_%d", id);
+    }
+
+    @NonNull
+    private static String getEmail(@NonNull String username) {
+        return String.format("%s@mail.com", username);
+    }
+
+    @NonNull
+    private User getUserForInserting(@NonNull String username, @NonNull Role role) {
 
         return new User(
                 null,
                 username,
                 username,
                 username,
-                String.format("karma-app.%s@gmail.com", username),
+                getEmail(username),
                 bCryptPasswordEncoder.encode(username),
-                Role.USER,
+                role,
                 null,
                 null
         );
     }
 
+
+    @Value("${initialize.data}")
+    private String shouldInitData;
+
     @Override
     public void run(String... args) {
+
+        if (!shouldInitData.equalsIgnoreCase("true")) {
+            return;
+        }
+
+        Optional<User> optionalAdmin = userRepository.findByUsername(ADMIN_USERNAME);
+        if (optionalAdmin.isPresent()) {
+            logger.info("Data initialization has been already performed.");
+            return;
+        }
+
+        userRepository.save(getUserForInserting(ADMIN_USERNAME, Role.ADMIN));
+        userRepository.save(getUserForInserting(MOD_USERNAME, Role.MOD));
 
         var random = new Random();
 
@@ -57,7 +97,7 @@ public class UserAndPostInit implements CommandLineRunner {
             int currLow = insertionIdx * MAX_SINGLE_INSERT_SIZE;
             int currHigh = Math.min((insertionIdx + 1) * MAX_SINGLE_INSERT_SIZE, USER_AMOUNT);
             for (int userId = currLow; userId < currHigh; userId++) {
-                var user = getUserForInserting(String.format("username_%d", userId));
+                var user = getUserForInserting(getUsername(userId), Role.USER);
 
                 var maxPosts = random.nextInt(MAX_POSTS_PER_USER - MIN_POSTS_PER_USER) + MIN_POSTS_PER_USER;
 
@@ -80,6 +120,7 @@ public class UserAndPostInit implements CommandLineRunner {
             postRepository.saveAll(postsToSave); // Save posts in batch
         }
 
-        System.out.println("DONE");
+        logger.info("Data initialization is done.");
     }
+
 }
